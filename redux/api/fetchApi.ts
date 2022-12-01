dc004a26b9e0529f507f7a056562a29e7c0a2ab4
unknown <milosz20031207@gmail.com>
2022-12-01 01:21:57 +0100

    initial commit

--- redux/api/fetchApi.ts
+++ redux/api/fetchApi.ts
@@ -17,7 +17,7 @@ export const fetchApi = createApi( {
     reducerPath: 'api',
     tagTypes: [ "refresh", "category", "voted", "joined", "commented" ],
     baseQuery: graphqlBaseQuery( { 
-        baseUrl: 'graphql' ,
+        baseUrl: 'graphiql' ,
     } ),
     endpoints: ( { mutation, query } ) => ({
         getHello: query<any, queryType>( {
