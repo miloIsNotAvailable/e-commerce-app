ccd0bcac95a493c996361fcad665b9549ce379f5
unknown <milosz20031207@gmail.com>
2022-12-07 18:35:40 +0100

    layout: added sign up layout

--- components/navbar/menu/SignInModal/SignInModal.tsx
+++ components/navbar/menu/SignInModal/SignInModal.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, MouseEvent, useState } from "react";
 import { useModalContext } from "../../../../contexts/ModalContext";
 import Button from "../../../custom/Button";
 import { styles } from "../../build/NavbarStyles";
@@ -7,9 +7,16 @@ import Form from "./Form";
 const SignInModal: FC = () => {
 
     const [ open, setOpen ] = useModalContext()
+    const [ signUp, setSignUp ] = useState<boolean>( false )
 
     const handleCloseModal: () => void = () => {
         setOpen( !open )
+    }
+
+    const handleSignUp: ( e: MouseEvent<HTMLButtonElement> ) => void 
+    = e => {
+        e.preventDefault()
+        setSignUp( !signUp )
     }
 
     return (
@@ -28,9 +35,20 @@ const SignInModal: FC = () => {
                 placeholder={ "password" } 
                 type={ "password" }
             />
+            {
+                signUp && 
+                <Form 
+                    placeholder={ "username" } 
+                    type={ "username" }
+                />
+            }
             <div className={ styles.modal_buttons }>
                 <Button>sign in</Button>
-                <Button>sign up</Button>
+                <Button
+                    onClick={ handleSignUp }
+                >
+                    {signUp ? "back" : "sign up"}
+                </Button>
             </div>
         </form>
     )
