a092a1e9cd8e9cec19cc6ee09189d11a651b1e3f
unknown <milosz20031207@gmail.com>
2022-12-15 13:59:23 +0100

    feat: added apollo server

--- api/graphql.ts
+++ api/graphql.ts
@@ -1,13 +1,29 @@
-import { graphqlHTTP } from 'express-graphql'
-import { schema } from '../graphql/schema/schema' 
-import { root } from '../graphql/resolvers/resolvers' 
+import { Request, Response } from "express";
+// import { graphqlHTTP } from 'express-graphql'
+// import { schema } from '../graphql/schema/schema' 
+// import { root } from '../graphql/resolvers/resolvers' 
 
-export default graphqlHTTP( async( req: any, res ) => {
+// export default graphqlHTTP( async( req: any, res ) => {
   
-      return {
-        schema,
-        rootValue: root,
-        graphiql: true,
-        context: { req, res },
-      }
-})
+//       return {
+//         schema,
+//         rootValue: root,
+//         graphiql: true,
+//         context: { req, res },
+//       }
+// })
+
+// import { prisma } from "../prisma/client";
+
+export default async function handler( req: Request, res: Response ) {
+
+    try {
+
+        // const data = await prisma.users.findMany( {} );
+        // console.log( data )
+        
+        res.send( { name: 'john Doe' } )
+
+    } catch( e ) {  console.log( e ) }
+
+}
