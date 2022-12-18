d5d0602549b2fa101c95bb35e9994b23e913ea47
unknown <milosz20031207@gmail.com>
2022-12-18 17:30:34 +0100

    fix: fixed cookies not getting sent in production

--- server/build.ts
+++ server/build.ts
@@ -8,22 +8,23 @@ import express from "express";
 import http from "http";
 import { root } from "../graphql/resolvers/resolvers";
 import { schema, schema_ } from "../graphql/schema/schema";
+import { ApolloServerPluginLandingPageGraphQLPlayground } from '@apollo/server-plugin-landing-page-graphql-playground' 
 
 export const app = express();
 export const httpServer = http.createServer(app);
 
 const ApolloServerLandingPage = () =>
-//   process.env.NODE_ENV === "production"
-//     ? ApolloServerPluginLandingPageProductionDefault({
-//         embed: true,
-//         graphRef: "ecommerce-stuff@current",
-//         includeCookies: true,
-//       })
-//     : 
-    ApolloServerPluginLandingPageLocalDefault({
-        includeCookies: true,
+//   process.env.NODE_ENV !== "production"? 
+  ApolloServerPluginLandingPageProductionDefault({
         embed: true,
-      });
+        graphRef: "ecommerce-stuff@current",
+        includeCookies: true,
+      })
+    // : 
+    // ApolloServerPluginLandingPageLocalDefault({
+    //     includeCookies: true,
+    //     embed: true,
+    //   });
 
 export const server = new ApolloServer<any>({
   typeDefs: schema,
@@ -31,6 +32,7 @@ export const server = new ApolloServer<any>({
   resolvers: root,
   plugins: [
     ApolloServerPluginDrainHttpServer({ httpServer }),
-    ApolloServerLandingPage(),
+    ApolloServerPluginLandingPageGraphQLPlayground(),
+    // ApolloServerLandingPage(),
   ],
 });
