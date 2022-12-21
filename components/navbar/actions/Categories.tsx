ae282091bd879fc7a530b701017aca33c3c2dc01
unknown <milosz20031207@gmail.com>
2022-12-21 19:16:51 +0100

    layout: added categories popup menu

--- components/navbar/actions/Categories.tsx
+++ components/navbar/actions/Categories.tsx
@@ -4,8 +4,14 @@ import { styles } from "../build/NavbarStyles";
 const Categories: FC = () => {
 
     return (
-        <div className={ styles.action }>
+        <div className={ styles.categories } tabIndex={ 0 }>
             categories
+            <ul className={ styles.categories_menu }>
+                <li>hey</li>
+                <li>hey</li>
+                <li>hey</li>
+                <li>hey</li>
+            </ul>
         </div>
     )
 }
