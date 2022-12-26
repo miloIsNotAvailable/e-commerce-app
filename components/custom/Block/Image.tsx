5a3316c7aef7bb8af53fab146b3e99b1331090c5
unknown <milosz20031207@gmail.com>
2022-12-27 00:40:16 +0100

    layout: added setting background color to most common image color

--- components/custom/Block/Image.tsx
+++ components/custom/Block/Image.tsx
@@ -1,4 +1,5 @@
-import { FC } from "react";
+import { RGB, useAvgBgColor } from "@hooks/useAvgBgColor";
+import { FC, useEffect, useRef, useState } from "react";
 import { styles } from "./BlockStyles";
 
 interface ImageProps {
@@ -7,9 +8,43 @@ interface ImageProps {
 
 const Image: FC<ImageProps> = ( { src } ) => {
 
+    const imgRef = useRef<HTMLImageElement| null>( null )
+    const getCol = useAvgBgColor( )
+    const [ color, setColor ] = useState<RGB>( { r: 255, g: 144, b: 173 } )
+
+    useEffect( () => {
+        if( !imgRef.current ) return
+        
+        var canvas = document.createElement("canvas");
+    
+        canvas.width = imgRef.current.width;
+        canvas.height = imgRef.current.height;
+        
+        var ctx = canvas.getContext("2d");
+        
+        if( !ctx ) return
+        ctx.drawImage(imgRef.current, 0, 0);
+    
+        var dataURL = canvas.toDataURL("image/png");
+
+        const img_ = document.createElement( "img" )
+        
+        img_.crossOrigin = "anonymous"
+        img_.src = dataURL
+        img_.width = imgRef.current.width 
+        img_.height = imgRef.current.height 
+
+        setColor(getCol( img_ ) )
+    }, [ imgRef.current ] )
+
     return <img 
         className={ styles.img }
+        crossOrigin={ "anonymous" }
+        ref={ imgRef }
         src={ src }
+        style={ {
+            backgroundColor: `rgb(${ Object.values( color ).join( "," ) })`
+        } }
     />
 }
 
---
+++ hooks/useAvgBgColor.ts
@@ -0,0 +1,57 @@
+import { FastAverageColor } from "fast-average-color";
+
+export type RGB = {
+    r: number;
+    g: number;
+    b: number;
+}
+
+export const useAvgBgColor = () => {
+
+    return ( imgEl: HTMLImageElement | null ): RGB => {
+        
+        if( !imgEl ) return { r: 255, g: 144, b: 173 }
+
+        var blockSize = 5, // only visit every 5 pixels
+            defaultRGB = { r: 255, g: 144, b: 173 }, // for non-supporting envs
+            canvas = document.createElement('canvas'),
+            context = canvas.getContext && canvas.getContext('2d'),
+            data, width, height,
+            i = -4,
+            length,
+            rgb = { r: 255, g: 144, b: 173 },
+            count = 0;
+    
+        if (!context) {
+            return defaultRGB;
+        }
+    
+        height = canvas.height = imgEl.naturalHeight || imgEl.offsetHeight || imgEl.height;
+        width = canvas.width = imgEl.naturalWidth || imgEl.offsetWidth || imgEl.width;
+    
+        context.drawImage(imgEl, 0, 0);
+    
+        try {
+            data = context.getImageData(0, 0, width, height);
+        } catch(e) {
+            /* security error, img on diff domain */
+            return defaultRGB;
+        }
+    
+        length = data.data.length;
+    
+        while ( (i += blockSize * 4) < length ) {
+            ++count;
+            rgb.r += data.data[i];
+            rgb.g += data.data[i+1];
+            rgb.b += data.data[i+2];
+        }
+    
+        // ~~ used to floor values
+        rgb.r = ~~(rgb.r/count);
+        rgb.g = ~~(rgb.g/count);
+        rgb.b = ~~(rgb.b/count);
+    
+        return rgb;   
+    }
+}
