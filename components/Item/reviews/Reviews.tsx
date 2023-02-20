ad2718ca42ae33744d588c1ecae26291b938806a
unknown <milosz20031207@gmail.com>
2023-02-20 21:02:53 +0100

    layout: added loading screen for reviews and account icon next to a review

--- components/Item/reviews/Reviews.tsx
+++ components/Item/reviews/Reviews.tsx
@@ -3,6 +3,7 @@ import { GetReviewsQuery } from "@graphql-types";
 import { gql } from "graphql-request";
 import { FC, useEffect } from "react";
 import { useGetReviewsQuery, useLazyGetReviewsQuery } from "../../../redux/api/fetchApi";
+import Loader from "../../custom/Loader";
 import { styles } from "../build/ItemStyles";
 import ReviewsInput from "./ReviewsInput";
 
@@ -32,16 +33,29 @@ const Reviews: FC = () => {
 
     }, [ getItem, itemDataLoading ] )
 
+    if( !isSuccess ) return (
+        <div className={ styles.reviews_wrap }>
+            <div className={ styles.reviews_header }>
+                reviews
+            </div>
+            <div className={ styles.review_loading }>
+                <Loader/> 
+            </div>
+            <ReviewsInput/>
+        </div>
+    )
+
     return (
         <div className={ styles.reviews_wrap }>
             <div className={ styles.reviews_header }>
                 reviews
             </div>
-            <div className={ styles.review_wrap } >
+            <div className={ styles.review_wrap }>
                 { isSuccess && data?.getReviews && 
                 data!.getReviews!.map( ( { text } ) => (
                     <div className={ styles.review }>
-                        { text }
+                        <div className={ styles.review_icon }></div>
+                        <div>{ text }</div>
                     </div>
                 ) ) }
             </div>
